548a5f75abdbe2aaa4b4aee7f1bff4e9585f26ee
Ajay Kumar <109513537+Ajaykumar-1201@users.noreply.github.com>
2023-08-29 15:49:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  if (num === 0) {
+        return "0"; // Special case for 0
+    }
+
+    let binary = "";
+    while (num > 0) {
+        const remainder = num % 2;
+        binary = remainder + binary; // Prepend the remainder to the binary representation
+        num = Math.floor(num / 2);
+    }
+
+    return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
